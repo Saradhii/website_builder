672ed0e443a08636e0a29554f20116634714338a
Saradhi <99667252+Saradhii@users.noreply.github.com>
2024-07-30 10:00:26 +0530

    Update chatForm.tsx

--- my-next-app/components/chatForm.tsx
+++ my-next-app/components/chatForm.tsx
@@ -10,7 +10,6 @@ export default function ChatForm() {
               className="absolute inset-0 h-full w-full object-cover"
             />
           </aside>
-
           <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
             <div className="max-w-xl lg:max-w-3xl">
               <a className="block text-blue-600" href="#">
