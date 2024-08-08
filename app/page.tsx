c149e427dcd2686000e35997c2c3029347bcc4de
Saradhi <99667252+Saradhii@users.noreply.github.com>
2024-08-08 11:53:51 +0530

    new config

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,59 @@
+// import { Command } from "lucide-react";
+// import Link from "next/link";
+// import { DarkModeToggle } from "@/components/dark-mode-toggle";
+// import { RetroGridDemo } from "@/components/retro-grid-demo";
+// import { AnimatedListDemo } from "@/components/animated-list-demo";
+// import { DockDemo } from "@/components/dock-demo";
+
+// export default function Index() {
+//   return (
+//     <section className="w-full min-h-screen flex flex-col">
+//       <nav className="flex items-center justify-between p-4 w-full">
+//         <Link href="/" className="flex items-center space-x-2">
+//           <Command className="h-8 w-8" />
+//           <h1 className="text-xl font-semibold">Next + Magic-ui</h1>
+//         </Link>
+//         <DarkModeToggle />
+//       </nav>
+//       <div className="container flex justify-center px-4 md:px-6 flex-1 py-8 overflow-x-hidden">
+//         <div className="flex flex-col items-center space-y-4 text-center p-4 md:w-1/2">
+//           <h1 className="text-3xl md:text-4xl font-bold tracking-tighter">
+//             A{" "}
+//             <span className="font-extrabold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
+//               Simple Starter
+//             </span>{" "}
+//             For Nextjs, Shadcn-ui and{" "}
+//             <Link
+//               href="https://magicui.design/"
+//               className="hover:text-blue-500"
+//             >
+//               Magic-ui
+//             </Link>
+//           </h1>
+
+//           <div className="px-4 w-full">
+//             <RetroGridDemo />
+//           </div>
+//           <div className="px-4 w-full">
+//             <AnimatedListDemo />
+//           </div>
+//           <div className="px-4 w-full">
+//             <DockDemo />
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+
 'use client';
 import Image from "next/image";
-import ChatForm from "@/components/chatForm";
-import UserInput from "@/components/userInput";
 import { Content, GoogleGenerativeAI } from '@google/generative-ai';
 import axios from 'axios';
 import React, { useState, FormEvent, ChangeEvent } from 'react';
-import { marked } from "marked";
 import dotenv from "dotenv";
-import MyLottieAnimation from "@/components/MyLottieAnimation";
+import { RetroGridDemo } from "@/components/retro-grid-demo";
+// import MyLottieAnimation from "@/components/MyLottieAnimation";
 dotenv.config();
 
 export default function Home() {
@@ -89,7 +135,7 @@ export default function Home() {
                     caretColor: 'black',
                   }}
                 />
-                { isLoading ? <div className="h-10 w-10"><MyLottieAnimation/></div> : <button
+                { isLoading ? <div className="h-10 w-10"></div> : <button
                   type="submit"
                   className="ml-2 p-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
                   aria-label="Submit"
@@ -106,8 +152,10 @@ export default function Home() {
         </div>
       </div>
       <div className="w-1/2 bg-green-200 flex items-center justify-center">
+      <RetroGridDemo />
         <div dangerouslySetInnerHTML={{ __html: htmlToRender }}></div>
       </div>
     </div>
   );
 }
+
