1b5ee121205f164e0a1c355e52361987890e70fe
Saradhi <99667252+Saradhii@users.noreply.github.com>
2024-09-05 17:19:05 +0530

    added dev mode feature + rebase needed

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,10 @@ export default function Home() {
   const [textContent, setTextContent] = useState<string>('');
   const [history, setHistory] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState<boolean | null>(null);
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = (event: any) => {
+    setIsChecked(event.target.checked);
+  };
   const apiKey = process.env.GEMINI_API_KEY || "AIzaSyBoWVLiSCXKk69y6LbmN1UvY0suBI1l2Tg";
   const genAI = new GoogleGenerativeAI(`${apiKey}`);
 
@@ -81,7 +85,7 @@ export default function Home() {
         <div className="h-[70%] bg-blue-200 flex flex-col shadow-2xl">
           <div className="h-[10%] w-full flex justify-end p-2">
             <label className="inline-flex items-center cursor-pointer">
-              <input type="checkbox" value="" className="sr-only peer" />
+              <input type="checkbox" value="" className="sr-only peer" onChange={handleCheckboxChange} />
               <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300 mr-2">Dev Mode</span>
               <div className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-[#ff2975]">
               </div>
@@ -91,7 +95,14 @@ export default function Home() {
             {isLoading == null ? <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-7xl font-bold leading-none tracking-tighter text-transparent">
               Your AI powered <br></br>
               website builder
-            </span> : <pre>{textContent}</pre>}
+            </span> : isChecked ? <div className="p-4 mt-32">
+              <pre className="bg-gray-800 text-white rounded p-2 text-sm w-[690px] h-[600px] overflow-auto">
+                <code className="text-[10px]">{htmlToRender}</code>
+              </pre>
+            </div> : <div className="text-center text-sm font-mono text-gray-800 p-4">
+              {textContent}
+            </div>
+            }
           </div>
         </div>
         <div className="h-[30%] bg-blue-300 flex items-end justify-center">
@@ -104,6 +115,12 @@ export default function Home() {
                   rows={1}
                   value={inputValue}
                   onChange={handleInputChange}
+                  onKeyDown={(e: any) => {
+                    if (e.key === 'Enter' && !e.shiftKey) {
+                      e.preventDefault();
+                      handleSubmit(e);
+                    }
+                  }}
                   style={{
                     caretColor: 'black',
                   }}
@@ -114,7 +131,6 @@ export default function Home() {
                       <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
                       <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                     </svg>
-                    <span className="sr-only">Loading...</span>
                   </div>
                 </div> : <button
                   type="submit"
