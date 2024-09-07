143c4438edb3930d164d61ecd74f80f166c3957f
Saradhi <99667252+Saradhii@users.noreply.github.com>
2024-09-07 12:44:05 +0530

    added code editor for html render

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,9 @@ import Ripple from "@/components/magicui/ripple";
 import ShineBorder from "@/components/magicui/shine-border";
 import { useTheme } from "next-themes";
 import Safari from "@/components/magicui/safari";
+import CodeMirror from '@uiw/react-codemirror';
+import { html } from '@codemirror/lang-html';
+import { oneDark } from '@codemirror/theme-one-dark';
 // import MyLottieAnimation from "@/components/MyLottieAnimation";
 dotenv.config();
 
@@ -19,7 +22,7 @@ export default function Home() {
   const [requesting, setRequesting] = useState("not requesting");
   const [inputValue, setInputValue] = useState<string>('');
   const [lllmResponse, setLLLMResponse] = useState<string>('');
-  const [htmlToRender, setHtmlToRender] = useState<string>('');
+  const [htmlToRender, setHtmlToRender] = useState<string>(``);
   const [textContent, setTextContent] = useState<string>('');
   const [history, setHistory] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState<boolean | null>(null);
@@ -72,7 +75,6 @@ export default function Home() {
       setHtmlToRender(jsonResponse.htmlCode);
       setTextContent(jsonResponse.plainText);
       setRequesting("completed");
-      console.log(htmlToRender);
     } catch (err) {
       console.log("error generating content", err);
     }
@@ -83,35 +85,41 @@ export default function Home() {
   };
   return (
     <div className="flex h-screen w-screen">
-      <div className="w-full bg-blue-100 flex flex-col">
+      <div className={`${isLoading == null ? "w-full" :"w-1/2"} bg-blue-100 flex flex-col`}>
         <div className="h-[70%] bg-blue-200 flex flex-col shadow-2xl">
-          <div className="h-[5%] w-full flex justify-end p-2">
-            {requesting == "completed" ? <label className="inline-flex items-center cursor-pointer">
+          <div className="h-[5%] flex justify-end p-2">
+              {requesting == "completed" ? <label className="inline-flex items-center cursor-pointer">
               <input type="checkbox" value="" className="sr-only peer" onChange={handleCheckboxChange} />
               <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300 mr-2">Dev Mode</span>
               <div className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-[#ff2975]">
               </div>
-            </label> : null}
+            </label>  : null}
           </div>
           <div className="h-[95%] w-full flex items-center justify-center">
             {
-              isLoading == null ?
-                <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-7xl font-bold leading-none tracking-tighter text-transparent">
-                  Your AI powered <br></br>
-                  website builder
-                </span> :
-                isChecked ? <div className="w-[100%] h-[100%] text-sm bg-gray-800 text-white overflow-auto whitespace-pre-line p-5">
-                  <code className="font-mono">
-                    <textarea
-                      className="w-[100%] h-[100%] text-sm bg-gray-800 text-white overflow-auto whitespace-pre-line p-5"
-                      value={htmlToRender}
-                      onChange={(e) => setHtmlToRender(e.target.value)}
-                    ></textarea>
-
-                  </code>
-                </div> : <div className="text-center text-sm font-mono text-gray-800 p-4">
-                  {textContent}
-                </div>
+            isLoading == null ? 
+            <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-7xl font-bold leading-none tracking-tighter text-transparent">
+              Your AI powered <br></br>
+              website builder
+            </span> : 
+            isChecked ? 
+            // <div className="w-[100%] h-[100%] text-sm bg-gray-800 text-white overflow-auto whitespace-pre-line p-5">
+            //     <code className="font-mono">{htmlToRender}</code>
+            //   </div> 
+            <div className="w-[100%] h-[100%] overflow-auto">
+            <CodeMirror
+                     height="100%"
+                     width="100%"
+                     extensions={[html()]}
+                     theme={oneDark}
+                    value={htmlToRender}
+                    onChange={(newValue) => setHtmlToRender(newValue)}
+                    
+            />
+            </div>
+              : <div className="text-center text-sm font-mono text-gray-800 p-4">
+              {textContent}
+            </div>
             }
           </div>
         </div>
@@ -156,35 +164,11 @@ export default function Home() {
             <div>
             </div>
           </div>
-          {/* <NeonGradientCard className="w-5/6 h-3/6 rounded-t-lg flex flex-col shadow-2xl">
-          <form className="flex items-center" onSubmit={handleSubmit}>
-                <textarea
-                  className="h-full w-full resize-none border-none outline-none focus:ring-0 text-gray-700 placeholder-black bg-none"
-                  placeholder="Describe your website..."
-                  rows={1}
-                  value={inputValue}
-                  onChange={handleInputChange}
-                  style={{
-                    caretColor: 'black',
-                  }}
-                />
-                { isLoading ? <div className="h-10 w-10"><MyLottieAnimation/></div> : <button
-                  type="submit"
-                  className="ml-2 p-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-                  aria-label="Submit"
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" />
-                  </svg>
-                </button>}
-              </form>
-          </NeonGradientCard> */}
         </div>
       </div>
-      {isLoading !== null ? <div className="w-[90%] bg-green-200 flex items-center justify-center">
+      {isLoading !== null ? <div className="w-1/2 bg-green-200 flex items-center justify-center">
         {isLoading ? <RetroGridDemo /> : <div dangerouslySetInnerHTML={{ __html: htmlToRender }}></div>}
       </div> : null}
     </div>
   );
 }
-
