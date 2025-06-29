3782036b8ded1279893ad5386cab4612ee5ea7c5
Saradhi <99667252+Saradhii@users.noreply.github.com>
2025-06-29 12:44:35 +0530

    removed dark styles

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,7 @@ export default function Home() {
           <div className="h-[5%] flex justify-end p-2">
             {requesting == "completed" ? <label className="inline-flex items-center cursor-pointer">
               <input type="checkbox" value="" className="sr-only peer" onChange={handleCheckboxChange} />
-              <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300 mr-2">Dev Mode</span>
+              <span className="ms-3 text-sm font-medium text-gray-900 mr-2">Dev Mode</span>
               <div className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-[#ff2975]">
               </div>
             </label> : null}
